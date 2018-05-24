d8bf7643fc1e31ba7f0a138ad7a55a8b9daede41
Unknown <bastien.blanchard+github@gmail.com>
2018-05-24 16:13:26 +0200

    Gestion du formulaire dans le code timescript

--- src/app/items/containers/add-item/add-item.component.ts
+++ src/app/items/containers/add-item/add-item.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { State } from '../../state.enum';
+import {Component, OnInit} from '@angular/core';
+import {State} from '../../state.enum';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-item',
@@ -7,26 +9,39 @@ import { State } from '../../state.enum';
   styleUrls: ['./add-item.component.css']
 })
 export class AddItemComponent implements OnInit {
-  public State = State;
-  public name: string;
-  public reference: number;
-  public state: State;
-  public possibleStateValueList: string[] = [];
+  public form: FormGroup;
+  public possibleStateValueList: string[] = Object.values(State);
+  public nameLength = 0;
 
-  constructor() {
-    for (const state in State) {
-      if (State.hasOwnProperty(state)) {
-        this.possibleStateValueList.push(State[state]);
-      }
-    }
+  constructor(public fb: FormBuilder) {
   }
 
-  ngOnInit() {
-    this.state = State.ALIVRER;
+  public ngOnInit() {
+    this.createForm();
+    this.form.get('name').valueChanges.pipe(
+      debounceTime(100),
+      distinctUntilChanged()
+    ).subscribe((value) => this.nameLength = value.length);
+    this.form.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    ).subscribe((value) => console.log(value));
   }
 
-  public process(form) {
-    console.log(form.value);
+  public createForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      reference: ['', [Validators.required, Validators.minLength(4), Validators.pattern(/^\d+$/)]],
+      state: [State.ALIVRER, Validators.required]
+    });
   }
 
+  public process() {
+    // persister les données avec un appel http/Web Service
+    this.form.reset();
+  }
+
+  public isError(fieldName: string) {
+    return this.form.touched && this.form.get(fieldName).invalid;
+  }
 }
